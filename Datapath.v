a3ded4eb396c198db381801ac4d5bb5bf276536b
Zachary Golieb <35204601+Atlas213@users.noreply.github.com>
2019-04-10 09:33:17 -0400

    Update Datapath.v
    This is my most updated version of the Datapath. Im going to push a version of the CU that implements this datapath. Your free to implement this, or edit the CU to support your datapath

--- Datapath.v
+++ Datapath.v
@@ -1,8 +1,9 @@
-module Datapath(SA,SB,DA,WR,RST,CLK,FS,C0,STAT,K,M,EN_ALU,EN_ADDR_ALU,EN_B,r0, r1, r2, r3, r4, r5, r6, r7,RCS,RWE,ROE);
+module Datapath(SA,SB,DA,WR,IR,RST,CLK,FS,C0,STAT,K,M,EN_ALU,EN_ADDR_ALU,EN_B,EN_PC,EN_ADDR_PC,PC_SEL,PS,r0, r1, r2, r3, r4, r5, r6, r7,RCS,RWE,ROE,CONSIG);
 	input [4:0] SA; //ABUS register select
 	input [4:0] SB; //BBUS register select
 	input [4:0] DA; //Data input regiter select
 	input WR; //Write enable
+	input IR;
 	input RST; // Reset
 	input CLK; // Clock
 	input [4:0] FS;
@@ -13,6 +14,10 @@ module Datapath(SA,SB,DA,WR,RST,CLK,FS,C0,STAT,K,M,EN_ALU,EN_ADDR_ALU,EN_B,r0, r
 	input EN_ALU;
 	input EN_ADDR_ALU;
 	input EN_B; //ENABLE BBUS TO DBUS
+	input EN_PC;
+	input EN_ADDR_PC;
+	input PC_SEL;
+	input [1:0] PS;
 	output [15:0] r0, r1, r2, r3, r4, r5, r6, r7;
 	input RCS,RWE,ROE;
 	wire [63:0] ABUS;
@@ -21,14 +26,23 @@ module Datapath(SA,SB,DA,WR,RST,CLK,FS,C0,STAT,K,M,EN_ALU,EN_ADDR_ALU,EN_B,r0, r
 	wire [63:0] BBUS;
 	wire [63:0] MBUS; //Mux bus
 	wire [7:0] 	RABUS;
+	wire [31:0] PCBUS;
+	wire [29:0] IPCBUS;
+	output [31:0] CONSIG;
 	
 	RegisterFile32x64 RegFile	(ABUS,BBUS,SA,SB,DBUS,DA,WR,RST,CLK,r0, r1, r2, r3, r4, r5, r6, r7);
 	mux2to1_64bit	DataMux (MBUS, M, K, BBUS);
+	mux2to1_64bit	PCMux (IPCBUS, PC_SEL, ABUS[29:0], K[29:0]);
 	ALU_LEGv8	ALU	(ABUS, MBUS, FS, C0,FBUS, STAT);
+	PC progcount (IPCBUS[29:0],PS,CLK,PCBUS,RST);
 	tri_buf tbufB (BBUS,DBUS,EN_B);
 	tri_buf tbufF (FBUS,DBUS,EN_ALU);
+	tri_buf tbufDPC(PCBUS,DBUS,EN_PC);
+	tri_buf tbufAPC(PCBUS,RABUS,EN_ADDR_PC);
 	tri_buf tbufR (FBUS,RABUS,EN_ADDR_ALU);
 	ram_sp_sr_sw RAM(CLK,RABUS,DBUS,RCS,RWE,ROE);
+	RegisterNbit InstructionRegister(CONSIG,DBUS,IR,RST,CLK);
+	defparam InstructionRegister.N = 32;
 
 
 
